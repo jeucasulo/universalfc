d58112d26ef357a2017bd0f4c8849c5dd9920a49
Jeú Guimarães Conrado Junior <jeucasulo@hotmail.com>
2018-07-20 20:18:52 -0300

    partk test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Universal FC
                 </div>
 
                 <div class="links">
